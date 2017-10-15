b7ce683d3ba07c5ed233431bab3e2527081499a0
mikatalk <mika.indie@gmail.com>
2017-10-15 15:46:38 -0700

    Fix nab bar issue on page load

--- src/App.vue
+++ src/App.vue
@@ -70,7 +70,7 @@ export default {
 
     invertedScrollRatio () {
       // match break point in _layout.scss
-      if (this.windowWidth > 1 && this.windowWidth > 600) {
+      if (this.windowWidth < 280 && this.windowWidth > 600) {
         return 0
       }
       return 1 - Math.min(1, this.scrollRatio * 2)
